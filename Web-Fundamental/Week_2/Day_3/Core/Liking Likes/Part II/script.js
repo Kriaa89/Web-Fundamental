45787fb8b3827787ad80d62014fb3a9e125090c1
Kriaa89 <a.kriaa0605@myu.imset.tn>
2024-12-25 13:31:07 +0100

    PART I && II

---
+++ Web-Fundamental/Week_2/Day_3/Core/Liking Likes/Part II/script.js
@@ -0,0 +1,17 @@
+
+//var likeButton = document.querySelector("likePost1")
+function likePost1(){
+    var likeCount = document.querySelector("#likes1");
+    likeCount.innerHTML ++;
+}
+
+
+function likePost2(){
+    var likeCount1 = document.querySelector("#likes2");
+    likeCount1.innerHTML ++;
+}
+
+function likePost3(){
+    var likeCount2 = document.querySelector("#likes3");
+    likeCount2.innerHTML ++;
+}
---
+++ Web-Fundamental/Week_2/Day_3/Core/Liking Likes/PartI/script.js
@@ -0,0 +1,5 @@
+function likePost1(){
+    likeCount.innerHTML ++;
+}
+
+var likeCount = document.querySelector("#likes1")
