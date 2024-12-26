f0155cf75fa22af8a5addeee96d12e4c75b2f2f3
Kriaa89 <a.kriaa0605@myu.imset.tn>
2024-12-26 17:29:26 +0100

    dojo weather && like connection

--- Web-Fundamental/Week_2/Day_3/Core/Liking Likes/Part II/script.js
+++ Web-Fundamental/Week_2/Day_3/Core/Liking Likes/Part II/script.js
@@ -1,17 +1,6 @@
 
 //var likeButton = document.querySelector("likePost1")
-function likePost1(){
-    var likeCount = document.querySelector("#likes1");
+function likePost1(id){
+    var likeCount = document.querySelector(id);
     likeCount.innerHTML ++;
-}
-
-
-function likePost2(){
-    var likeCount1 = document.querySelector("#likes2");
-    likeCount1.innerHTML ++;
-}
-
-function likePost3(){
-    var likeCount2 = document.querySelector("#likes3");
-    likeCount2.innerHTML ++;
 }
---
+++ Web-Fundamental/Week_2/Day_3/Practice/Making Connections/profile-page/script.js
@@ -0,0 +1,26 @@
+// clicking either button will remove the user from the request list
+// steps:
+// 
+
+
+// click on the edit icon to check if it works or no 
+function changeUserName(item){
+    document.querySelector(item).innerHTML = "abdallah yessine kriaa"
+}
+
+
+// function to increase by 1
+function accept(id){
+    var element = document.querySelector(id);
+    element.remove();
+    var requestCount = document.querySelector('.badge');
+    requestCount.innerHTML ++;
+}
+
+
+function dlete(id){
+    var element = document.querySelector(id);
+    element.remove();
+    var requestCount = document.querySelector('.badge');
+    requestCount.innerHTML --;
+}
